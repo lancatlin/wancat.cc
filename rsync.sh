0993cc80073ba999a24538a58cba59d45eaa24e4
wancat <wancat@wancat.cc>
2022-04-22 23:07:16 +0800

    Update rsync script location, remove matomo

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 hugo --cleanDestinationDir
 rsync --rsh="ssh -p2633" --exclude '.git' --delete \
 --chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
--rtvzP ./public/. pi@pi.wancat.cc:/var/www/wancat.cc/
+-rtvzP ./public/. wancat@linchpins.cloud:/var/www/wancat.cc/
