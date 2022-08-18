7cbea137cdf71a404f4448cec03f526836a2c50d
Justin Lin <wancat@wancat.cc>
2022-08-18 22:52:09 +0800

    Update deploy script: IPFS auto update dns link

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,28 @@
-#!/bin/bash
+#/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# Your server, I use ssh config
+SERVER=linchpins
+# Destination dir on server
+DEST=/var/www/wancat.cc/
+# Save your CloudFlare token somewhere. I use pass
+TOKEN=$(pass server/cloudflare-api)
+# Your domain id, get it by https://api.cloudflare.com/client/v4/zones
+ZONE="110a1c9faf67fbb31108d98a3d982a31"
+# Your dns record id, get it by https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records
+DNS="82afaac670192096d3e4d60798b723e2"
 
-cd public
+hugo --cleanDestinationDir
 
-git pull origin master
+rsync --exclude '.git' --delete \
+--chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
+-rtvzP ./public/. $SERVER:$DEST
 
-cd ..
-
-# Build the project.
-hugo --cleanDestinationDir # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
+IPFS=$(ssh linchpins "cd $DEST && ipfs add -r -Q .") &&
+echo $IPFS &&
+JSON_FMT='{"type":"TXT","name":"@","content":"dnslink=/ipfs/%s","ttl":3600,"proxied":false}' &&
+DATA=$(printf "$JSON_FMT" "$IPFS") &&
+echo $DATA &&
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/$DNS" \
+     -H "Authorization: Bearer $TOKEN" \
+     -H "Content-Type: application/json" \
+     --data "$DATA"
--- rsync.sh
+++
@@ -1,4 +0,0 @@
-hugo --cleanDestinationDir
-rsync --rsh="ssh -p2633" --exclude '.git' --delete \
---chmod=Du=rwx,Dgo=rx,Fu=rw,Fog=r \
--rtvzP ./public/. wancat@linchpins.cloud:/var/www/wancat.cc/
